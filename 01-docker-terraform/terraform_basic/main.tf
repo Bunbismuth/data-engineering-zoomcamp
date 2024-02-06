3349686ee9100682a00adadb1a603f54f39a3ab1
Thiago Barros <bunbismuth@proton.me>
2024-02-06 08:26:05 -0300

    refactor: some issues in the code comments

--- 01-docker-terraform/terraform_basic/main.tf
+++ 01-docker-terraform/terraform_basic/main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "google" {
     # Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
-    #  credentials =
+    # credentials =
     project = "<Seu project ID>"
     region = "us-central1" 
 }
 
 resource "google_storage_bucket" "data-lake-bucket" {
-    name = "<Seu nome de Bucket"
+    name = "<Seu nome de Bucket>"
     location = "US"
 
     # optional, but recommended settings:
